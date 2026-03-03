dcb09db76ea01dead43ce693a7bac0658ba86358
mstanton6 <mstanton6@yahoo.com>
2026-03-03 12:51:32 -0500

    Main Page Movie data displaying

--- pages/MainPage.jsx
+++ pages/MainPage.jsx
@@ -2,14 +2,17 @@ import { useState, useEffect } from 'react'
 
 export default function Main() {
 
+    const[movies, setMovies] = useState([])
+
     async function getMovies() {
         // fetch all the movies
         const url = "http://localhost:3001/api/movies"
 
         try {
             let response = await fetch(url);
-            const movie = response.json();
-            console.log(movie);
+            const movies = await response.json();
+            console.log(movies);
+            setMovies(movies);
         } catch (err) {
             console.error(err);
         }
@@ -33,6 +36,18 @@ export default function Main() {
                       <th>watched</th>
                     </tr>
                 </thead>
+                <tbody>
+                {movies.map((movie) => (
+                <tr  key={movie._id}>
+                   <td>{movie.title}</td>
+                   <td>{movie.genre}</td>
+                   <td>{movie.year}</td>
+                   <td>{movie.rating}</td>
+                   <td>{movie.watched}</td>
+                 </tr>
+                )
+                )}
+                </tbody>
             </table>
         </div>
     );
