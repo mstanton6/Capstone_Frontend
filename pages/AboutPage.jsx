bb19d9d99ed87d504a863a05e6c028ec5a69fb31
mstanton6 <mstanton6@yahoo.com>
2026-03-05 16:14:20 -0500

    About Page - About 1 image is working

--- pages/AboutPage.jsx
+++ pages/AboutPage.jsx
@@ -1,8 +1,11 @@
+import backfuture from "../src/assets/images/backtothefuture.jpg"
+
 export default function aboutPage() {
 
     return (
         <div>
             <h1>This is the About Page</h1>
+            <img src={backfuture}/>
         </div>
     );
     
