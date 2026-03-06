1ae0d6a5bd370be63a3d784ce715bf1c97b1f056
mstanton6 <mstanton6@yahoo.com>
2026-03-06 12:50:57 -0500

    About Page - done

--- pages/AboutPage.jsx
+++ pages/AboutPage.jsx
@@ -8,9 +8,10 @@ export default function AboutPage() {
 
     return (
         <div className="displayimage">
-            <h2>This is the About Page. A young man (Michael J. Fox) is accidentally sent thirty years into the past in a time-traveling DeLorean invented by his friend, Dr. Emmett Brown (Christopher Lloyd),
-                 and must make sure his high-school-age parents unite in order to save his own existence. Doc Brown's iconic "Roads? Where we're going, we don't need roads!" and Marty's "Great Scott!" or "This is heavy!", along with George McFly's "Nobody calls me chicken!" and the hilarious "Make like a tree and get outta here!". Key lines also involve 1.21 gigawatts, the DeLorean time machine, 
-                 and Marty's reactions to his parents' romance, showcasing memorable moments from the trilogy </h2>
+            <h1>About Page</h1>
+            <h2>This site is dedicated to movies and movie lovers. It helps you keep track of the films you’ve already watched and the ones you want to watch in the future.
+                From the main page, you can easily add new movies, edit existing movies, or remove movies from your list. The goal of this site is to provide a simple and convenient way to organize your personal movie collection and watchlist.
+            </h2>
             <br></br>
             <div className="images">
               <img src={backfuture} alt="Back to the Future" />
